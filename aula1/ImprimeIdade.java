d9bf235cb0d600b80ff44a67601576acd82b957e
lucaswotta <lucaswotta@gmail.com>
2022-07-13 00:55:30 -0300

    Parte 1 da Aula 1 Java

--- aula1/ImprimeIdade.java
+++ aula1/ImprimeIdade.java
@@ -5,25 +5,37 @@ public class ImprimeIdade {
         int idade = 28;
         String nome = "Lucas Motta";
         double altura = 1.93;
-        double peso = 97.690;
+        double peso = 92.690;
 
         double imc = (peso) / (altura * altura);
-        System.out.println("Olá " + nome);
-        
-        if(idade >= 18) {
-            if(imc < 22) {
-                System.out.println("Você tem " +idade+ " anos e seu imc é de " +imc+ " Cuidado! Você corre risco de subnutrição.");
-            } else if(imc <= 30) {
-                System.out.println("Você tem " +idade+ " anos e seu imc é de " +imc+ " Parabéns! Você está no peso ideal!");
-            } else if(imc <= 35){
-                System.out.println("Você tem " +idade+ " anos e seu imc é de " +imc+ " Atenção! Você está fora do peso ideal!");
+        System.out.println("Olá, " + nome);
+
+        if (idade >= 28) {
+            if (imc < 18) {
+                System.out.println("Você tem " + idade + " anos e seu imc é de " + imc
+                        + " Cuidado! Você corre risco de subnutrição.");
+            } else if (imc <= 25) {
+                System.out.println(
+                        "Você tem " + idade + " anos e seu imc é de " + imc + " Parabéns! Você está no peso ideal!");
+            } else if (imc <= 30) {
+                System.out.println("Você tem " + idade + " anos e seu imc é de " + imc
+                        + " Atenção! Você está fora do peso ideal!");
             } else {
-                System.out.println("Você tem " +idade+ " anos e seu imc é de " +imc+ " Cuidado! Você corre risco de obesidade");
-            }       
-        
-        }else {
-            System.out.println("Novo demais para medição");
+                System.out.println("Você tem " + idade + " anos e seu imc é de " + imc
+                        + " Cuidado! Você corre risco de obesidade");
+            }
+        } else {
         }
-        
+        if (imc <= 18) {
+            System.out.println("Procure um médico para avaliação de subnutrição.");
+        } else if (imc <= 25) {
+            System.out.println("Seu índice está controlado e saudável procure um nutricionista.");
+        } else if (imc <= 30) {
+            System.out.println("Parabéns consulte um médico para medição muscular.");
+        } else if (imc <= 40) {
+            System.out.println("Pré obesidade precisará de acompanhamento nutricional.");
+        } else {
+            System.out.println("Procure um médico endocrinologista com urgência.");
+        }
     }
 }
